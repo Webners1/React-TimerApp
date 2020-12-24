9ff11dc4bf2af52260649586f766af0a7e2ac455
Webners1 <Webners1>
2020-12-24 16:36:43 +0500

    TimerApp

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello world/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("home", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/home/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("setting", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/setting/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is Hello World
+      <h1>Home</h1>
+      <h1>Contact</h1>
+      <h1>Profile</h1>
+      <h1>Setting</h1>
     </div>
   );
 }
